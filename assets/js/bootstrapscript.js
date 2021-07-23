34f42a560cd297c254eb53c492ffb7648c9911a9
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-07-23 09:00:41 -0500

    Se agrega nuevas funcionalidades

--- assets/js/bootstrapscript.js
+++ assets/js/bootstrapscript.js
@@ -1,7 +1,22 @@
 //Variables
 
 let validar = document.getElementById("btnValidar");
+let result = 0;
 
+
+const validarFormularioBootstrap = () =>{
+    let input1 = document.getElementsByName("bootstrap");
+    let input2 = document.getElementsByName("integracion");
+    let input3 = document.getElementsByName("creacion");
+
+    if (validadorAlgo(input1, "b") && validadorAlgo(input2, "a") && validadorAlgo(input3, "d")) {
+        alert("Se tienen " + resultado + " de  3 respuestas correctas");
+    } else {
+        alert("faltan respuestas por marcar")
+        resultado = 0;
+    }
+
+};
 
 //Funciones
 
@@ -12,5 +27,5 @@ fnaValidar =()=>{
 //EVENTOS
 
 validar.onclick = function(){
-    fnaValidar();
+    validarFormularioBootstrap();
 }
