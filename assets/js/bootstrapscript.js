72784b484e737f78df54aa71e6b8e0c0ab8225f0
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-07-26 09:29:18 -0500

    Se soluciona el error

--- assets/js/bootstrapscript.js
+++ assets/js/bootstrapscript.js
@@ -5,17 +5,43 @@ let result = 0;
 
 
 const validarFormularioBootstrap = () =>{
-    let input1 = document.getElementsByName("bootstrap");
-    let input2 = document.getElementsByName("integracion");
-    let input3 = document.getElementsByName("creacion");
+    let $Q1 = document.getElementsByName("bootstrap");
+    let $Q2 = document.getElementsByName("integracion");
+    let $Q3 = document.getElementsByName("creacion");
 
-    if (validadorAlgo(input1, "b") && validadorAlgo(input2, "a") && validadorAlgo(input3, "d")) {
-        alert("Se tienen " + resultado + " de  3 respuestas correctas");
+    if (validadorAlgo($Q1, "c") && validadorAlgo($Q2, "a") && validadorAlgo($Q3, "c")) {
+        alert("Se tienen " + result + " de  3 respuestas correctas");
     } else {
         alert("faltan respuestas por marcar")
-        resultado = 0;
+        result = 0;
     }
 
+};
+
+const validadorAlgo = (Q, res) =>{
+    for (let i = 0; i < Q.length; i++) {
+        if(Q[i].checked){
+            if (compareQuestion(Q[i], res)) {
+                result += 1;
+            }
+            return true;
+        }
+    }
+    return false;
+};
+
+const compareQuestion = (Q, res) => {
+    let id = Q.id;
+    let question = document.getElementsByName(id)[0].innerText;
+    console.log(id);
+    console.log(question);
+    question = question.slice(0, 1);
+    console.log(question);
+    if (question === res) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 //Funciones
